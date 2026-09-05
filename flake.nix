0e9faacb6573daf7c4b575d5d3b05f3a5c8915c7
Pavel Anpin <pavel@anpin.fyi>
2026-09-05 13:03:37 +0200

    bump fod and use nixos 26.05

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
   # Common inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
     home-manager.url = "github:nix-community/home-manager";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
